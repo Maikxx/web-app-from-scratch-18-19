54b9ee74ba594cf75dfcb5994b62b94b83b2f7b3
Maikel van Veen <maikeloo10@hotmail.com>
2019-02-11 13:58:04 +0100

    refactor(sorters): place sorter in utils

--- client/src/ts/views/CharacterMasterView.ts
+++ client/src/ts/views/CharacterMasterView.ts
@@ -4,7 +4,7 @@ import { PageHeader } from '../components/Chrome/PageHeader'
 import { CharacterButton } from '../components/Character/CharacterButton'
 import { View } from '../components/Core/View'
 import { Router } from '../components/Core/Router'
-import { sortByObjectKey } from '../sorters/sortByObjectKey'
+import { sortByObjectKey } from '../utils/sortByObjectKey'
 
 interface Props {
     hook: HTMLElement
