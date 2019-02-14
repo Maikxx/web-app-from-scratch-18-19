607f61e40239bc903710fa52a83aa771adcc17f5
Maikel van Veen <maikeloo10@hotmail.com>
2019-02-14 19:41:40 +0100

    fix(query): fix pageSize to max

--- client/src/ts/fetchers/character.ts
+++ client/src/ts/fetchers/character.ts
@@ -3,7 +3,7 @@ import { Fetcher } from '../utils/Fetcher'
 export async function fetchCharacters() {
     const urlRoot = `https://anapioficeandfire.com/api/`
     const characterUrlRoot = `${urlRoot}/characters`
-    const charactersUrl = `${characterUrlRoot}/?pageSize=100000`
+    const charactersUrl = `${characterUrlRoot}/?pageSize=50`
 
     return new Fetcher({ url: charactersUrl }).fetch()
 }
